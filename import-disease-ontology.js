fa8ecd14fcaefca06169586c3e6970f9481e5d35
Brian Dunnette <bdunnette@gmail.com>
2016-03-23 19:33:06 -0500

    removing textangular

--- import-disease-ontology.js
+++ import-disease-ontology.js
@@ -15,12 +15,14 @@ var parseStanza = function(stanza, getStanzaType) {
     }
     if (stanzaObject.hasOwnProperty(key) && typeof stanzaObject[key] === 'object') {
       stanzaObject[key].push(value);
-    } else if (stanzaObject.hasOwnProperty(key)) {
+    }
+    else if (stanzaObject.hasOwnProperty(key)) {
       var keyVal = stanzaObject[key];
       stanzaObject[key] = [];
       stanzaObject[key].push(keyVal);
       stanzaObject[key].push(value);
-    } else {
+    }
+    else {
       stanzaObject[key] = value;
     }
   });
@@ -85,81 +87,82 @@ var parseStanza = function(stanza, getStanzaType) {
 //     ], function (err, res) {
 //         // Handle response
 //     });
-  
-// getOBOFile: function(oboUrl) {
-//     HTTP.get(oboUrl, function(err, response) {
-//       if (response.content) {
-//         Diseases.upsert({
-//           'oboUrl': oboUrl
-//         }, {
-//           $set: {
-//             'oboContent': response.content,
-//             'oboUrl': oboUrl
-//           }
-//         }, function(error, result) {
-//           return result;
-//         });
-//       }
-//     });
-//   },
-//
-//   parseOBOStanzas: function(oboUrl) {
-//     console.log('Parsing OBO file from ' + oboUrl)
-//     var oboFile = Diseases.findOne({
-//       oboUrl: oboUrl
-//     });
-//     if (!oboFile) {
-//       console.log(oboFile);
-//       return oboFile;
-//     }
-//     var oboContent = oboFile.oboContent;
-//     var stanzas = oboContent.split("\n\n");
-//     var ontology = parseStanza(stanzas[0]);
-//
-//     Diseases.update({
-//       'oboUrl': oboUrl
-//     }, {
-//       $set: {
-//         'stanzas': stanzas.slice(1)
-//       }
-//     }, function(error, result) {
-//       return result;
-//     });
-//   },
-//
-//   parseOBOHeader: function(oboUrl) {
-//     console.log('Parsing OBO file header from ' + oboUrl)
-//     var oboFile = Diseases.findOne({
-//       oboUrl: oboUrl
-//     });
-//     var stanzas = oboFile.stanzas;
-//     var ontology = parseStanza(stanzas[0]);
-//
-//     Diseases.update({
-//       'oboUrl': oboUrl
-//     }, {
-//       $set: {
-//         'ontology': ontology
-//       }
-//     }, function(err, result) {
-//       return result;
-//     });
-//   },
-//
-//   parseOBOTerms: function(oboUrl) {
-//     console.log('Parsing OBO file terms from ' + oboUrl)
-//     var oboFile = Diseases.findOne({
-//       oboUrl: oboUrl
-//     });
-//     var terms = oboFile.stanzas;
-//
-//     terms.forEach(function(stanza) {
-//       var term = parseStanza(stanza, true);
-//       delete term.type;
-//       Diseases.upsert({
-//         'id': term.id
-//       }, {
-//         $set: term
-//       });
-//     });
-//   }
+
+var getOBOFile = function(oboUrl) {
+  HTTP.get(oboUrl, function(err, response) {
+    if (response.content) {
+      Diseases.upsert({
+        'oboUrl': oboUrl
+      }, {
+        $set: {
+          'oboContent': response.content,
+          'oboUrl': oboUrl
+        }
+      }, function(error, result) {
+        return result;
+      });
+    }
+  });
+};
+
+
+var parseOBOStanzas = function(oboUrl) {
+  console.log('Parsing OBO file from ' + oboUrl);
+  var oboFile = Diseases.findOne({
+    oboUrl: oboUrl
+  });
+  if (!oboFile) {
+    console.log(oboFile);
+    return oboFile;
+  }
+  var oboContent = oboFile.oboContent;
+  var stanzas = oboContent.split("\n\n");
+  var ontology = parseStanza(stanzas[0]);
+
+  Diseases.update({
+    'oboUrl': oboUrl
+  }, {
+    $set: {
+      'stanzas': stanzas.slice(1)
+    }
+  }, function(error, result) {
+    return result;
+  });
+};
+
+var parseOBOHeader = function(oboUrl) {
+  console.log('Parsing OBO file header from ' + oboUrl);
+  var oboFile = Diseases.findOne({
+    oboUrl: oboUrl
+  });
+  var stanzas = oboFile.stanzas;
+  var ontology = parseStanza(stanzas[0]);
+
+  Diseases.update({
+    'oboUrl': oboUrl
+  }, {
+    $set: {
+      'ontology': ontology
+    }
+  }, function(err, result) {
+    return result;
+  });
+};
+
+var parseOBOTerms = function(oboUrl) {
+  console.log('Parsing OBO file terms from ' + oboUrl);
+  var oboFile = Diseases.findOne({
+    oboUrl: oboUrl
+  });
+  var terms = oboFile.stanzas;
+
+  terms.forEach(function(stanza) {
+    var term = parseStanza(stanza, true);
+    delete term.type;
+    Diseases.upsert({
+      'id': term.id
+    }, {
+      $set: term
+    });
+  });
+};
